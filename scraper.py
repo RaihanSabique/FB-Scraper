8444ec50bb099bedb5b142cf0c534de6772ad588
RaihanSabique <htraihan@gmail.com>
2020-01-28 21:20:35 +0600

    Profile extraction done

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import traceback
 import getpass
 from pprint import pprint
+import pandas as pd
 
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
@@ -46,6 +47,11 @@ PROFILE.set_preference("app.update.enabled", False)
 PROFILE.update_preferences()
 
 
+total_scrolls = 2500
+current_scrolls = 0
+scroll_time = 8
+facebook_https_prefix = "https://"
+
 class CollectPosts(object):
     """Collector of recent FaceBook posts.
            Note: We bypass the FaceBook-Graph-API by using a
@@ -67,7 +73,7 @@ class CollectPosts(object):
         # creating CSV header
         with open(self.dump, "w", newline='', encoding="utf-8") as save_file:
             writer = csv.writer(save_file)
-            writer.writerow(["Author", "uTime", "Text"])
+            writer.writerow(["Author", "uTime", "Text", "profileLink"])
 
     def strip(self, string):
         """Helping function to remove all non alphanumeric characters"""
@@ -105,6 +111,12 @@ class CollectPosts(object):
                     link.click()
                 except:
                     pass
+            links_addition = self.browser.find_elements_by_link_text("Continue Reading")
+            for link in links_addition:
+                try:
+                    link.click()
+                except:
+                    pass
             posts = self.browser.find_elements_by_class_name(
                 "userContentWrapper")
             poster_names = self.browser.find_elements_by_xpath(
@@ -113,6 +125,7 @@ class CollectPosts(object):
             for count, post in enumerate(posts):
                 # Creating first CSV row entry with the poster name (eg. "Donald Trump")
                 analysis = [poster_names[count].text]
+                analysis.append(poster_names[count].get_attribute('href'))
 
                 # Creating a time entry.
                 time_element = post.find_element_by_css_selector("abbr")
@@ -121,8 +134,8 @@ class CollectPosts(object):
 
                 # Creating post text entry
                 text = post.find_element_by_class_name("userContent").text
-                status = self.strip(text)
-                analysis.append(status)
+                # status = self.strip(text)
+                analysis.append(text)
 
                 # Write row to csv
                 writer.writerow(analysis)
@@ -152,30 +165,50 @@ class CollectPosts(object):
                     link.click()
                 except:
                     pass
+            links_addition = self.browser.find_elements_by_link_text("Continue Reading")
+            for link in links_addition:
+                try:
+                    link.click()
+                except:
+                    pass
             posts = self.browser.find_elements_by_class_name(
                 "userContentWrapper")
             poster_names = self.browser.find_elements_by_xpath(
                 "//a[@data-hovercard-referer]")
 
             for count, post in enumerate(posts):
-                # Creating first CSV row entry with the poster name (eg. "Donald Trump")
-                analysis = [poster_names[count].text]
+                try:
+                    # Creating first CSV row entry with the poster name (eg. "Donald Trump")
+                    analysis = [poster_names[count].text]
+                    url=poster_names[count].get_attribute('href')
+                    print(url)
+                    user_id=self.create_original_link(url)
+                    # gender=self.scrape_genger(user_id)
+                    # print("Gender=====>",gender)
+                    # analysis.append(poster_names[count].get_attribute('href'))
+                    # analysis.append(self.scrape_genger(url))
+                    # Creating a time entry.
+                    time_element = post.find_element_by_css_selector("abbr")
+                    utime = time_element.get_attribute("data-utime")
+                    analysis.append(utime)
+                    pprint(post)
+                    # Creating post text entry
+                    text = post.find_element_by_class_name("userContent").text
+                    pprint(text)
+                    # text2=post.find_element_by_class_name('text_exposed_show').text
+                    # print(text2)
+                    # status = self.strip(text)
+                    analysis.append(text)
+                    print(user_id)
+                    analysis.append(user_id)
+                    # gender=self.scrape_genger(user_id)
+                    # print("Gender=====>",gender)
 
-                # Creating a time entry.
-                time_element = post.find_element_by_css_selector("abbr")
-                utime = time_element.get_attribute("data-utime")
-                analysis.append(utime)
-                pprint(post)
-                # Creating post text entry
-                text = post.find_element_by_class_name("userContent").text
-                pprint(text)
-                # text2=post.find_element_by_class_name('text_exposed_show').text
-                # print(text2)
-                status = self.strip(text)
-                analysis.append(status)
-
-                # Write row to csv
-                writer.writerow(analysis)
+                    # Write row to csv
+                    writer.writerow(analysis)
+                except Exception as e:
+                    print("type error: " + str(e))
+                    print(traceback.format_exc())
 
     def collect(self, typ):
         if typ == "groups":
@@ -230,6 +263,125 @@ class CollectPosts(object):
             print(sys.exc_info()[0])
             exit()
 
+##### Scrape Gender##########
+
+    def check_height(self):
+        new_height = self.browser.execute_script("return document.body.scrollHeight")
+        return new_height != old_height
+
+    def scroll(self):
+        global old_height
+        current_scrolls = 0
+
+        while (True):
+            try:
+                if current_scrolls == total_scrolls:
+                    return
+
+                old_height = self.browser.execute_script("return document.body.scrollHeight")
+                self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                WebDriverWait(self.browser, scroll_time, 0.05).until(lambda driver: self.check_height())
+                current_scrolls += 1
+            except TimeoutException:
+                break
+
+        return
+
+    def scrape_data(self,user_id, scan_list, section, elements_path, save_status, file_names):
+        """Given some parameters, this function can scrap friends/photos/videos/about/posts(statuses) of a profile"""
+        about_information=[]
+        page = []
+
+        if save_status == 4:
+            page.append(user_id)
+
+        page += [user_id + s for s in section]
+
+        for i, _ in enumerate(scan_list):
+            try:
+                self.browser.get(page[i])
+
+                if (save_status == 0) or (save_status == 1) or (
+                        save_status == 2):  # Only run this for friends, photos and videos
+
+                    # the bar which contains all the sections
+                    sections_bar = self.browser.find_element_by_xpath("//*[@class='_3cz'][1]/div[2]/div[1]")
+
+                    if sections_bar.text.find(scan_list[i]) == -1:
+                        continue
+
+                if save_status != 3:
+                    self.scroll()
+
+                data = self.browser.find_elements_by_xpath(elements_path[i])
+
+                about_information.append(data[0].text.split("\n"))
+
+
+                # save_to_file(file_names[i], data, save_status, i)
+            except Exception:
+                print("Exception (scrape_data)", str(i), "Status =", str(save_status), sys.exc_info()[0])
+        return about_information
+
+    def scrape_genger(self,user_id):
+        try:
+            scan_list = [None] * 7
+            section = ["/about?section=overview", "/about?section=education", "/about?section=living",
+                       "/about?section=contact-info", "/about?section=relationship", "/about?section=bio",
+                       "/about?section=year-overviews"]
+            elements_path = ["//*[contains(@id, 'pagelet_timeline_app_collection_')]/ul/li/div/div[2]/div/div"] * 7
+            file_names = ["Overview.txt", "Work and Education.txt", "Places Lived.txt", "Contact and Basic Info.txt",
+                          "Family and Relationships.txt", "Details About.txt", "Life Events.txt"]
+            save_status = 3
+
+            results=self.scrape_data(user_id, scan_list, section, elements_path, save_status, file_names)
+            print(results)
+            gender=None
+            for i in range(len(results)):
+                for j in range(len(results[i])):
+                    if(results[i][j]=="Gender"):
+                        gender=results[i][j+1]
+                        break
+            print("About Section Done!")
+            return gender
+        except Exception as e:
+            print("type error: " + str(e))
+            print(traceback.format_exc())
+            return  None
+
+    def clean_link(self,url):
+        if('profile.php?' in url):
+            indx1=url.find("id=")
+            indx2=url.find('&fref')
+            id=url[indx1+len('id='):indx2]
+            u="https://www.facebook.com/"+id
+            return u
+        idx=url.find('?')
+        u=url[:idx]
+        return u
+
+    def create_original_link(self,url):
+        url=self.clean_link(url)
+        if url.find(".php") != -1:
+            original_link = facebook_https_prefix + ".facebook.com/" + ((url.split("="))[1])
+
+            if original_link.find("&") != -1:
+                original_link = original_link.split("&")[0]
+
+        elif url.find("fnr_t") != -1:
+            original_link = facebook_https_prefix + ".facebook.com/" + ((url.split("/"))[-1].split("?")[0])
+        elif url.find("_tab") != -1:
+            original_link = facebook_https_prefix + ".facebook.com/" + (url.split("?")[0]).split("/")[-1]
+        else:
+            original_link = url
+
+        return original_link
+
+    def retrive_gender(self,df):
+        for link in df['profileLink']:
+            print(link)
+            g=self.scrape_genger(self.create_original_link(link))
+            print(g)
 
 if __name__ == "__main__":
 
@@ -241,30 +393,71 @@ if __name__ == "__main__":
     #         print(
     #             "Your email or password is missing. Kindly write them in credentials.txt")
     #         exit()
+    f = open("input.txt", "r")
+
     file_dir=dir_path+"/Data/"
     email=input("Enter your email/username : ")
     password=getpass.getpass(prompt='Enter Password:')
-    while True:
+    url='https://www.facebook.com/MobassharRahman'
+    object=[]
+    for x in f:
         try:
-            type = input("Enter 1 for scraping from Group or Enter 2 for scraping from Page: ")
-            if(type=="1"):
-                group_name=input("Enter Group Name:")
-                print("Depth:")
-                depth=int(input())
-                file_name=input("Enter the filename you want to save:")
-                file_name=file_dir+file_name + ".csv"
-                C = CollectPosts(ids=group_name,corpus_file=file_name ,depth=depth)
-                C.login(email, password)
-                C.collect("groups")
-            if (type == "2"):
-                page_name = input("Enter Page Name:")
-                print("Depth:")
-                depth = int(input())
-                file_name = input("Enter the filename you want to save:")
-                file_name = file_dir+file_name + ".csv"
-                C = CollectPosts(ids=page_name,corpus_file=file_name ,depth=depth)
-                C.login(email, password)
-                C.collect("pages")
+            x=x.split()
+            print(x)
+            type=int(x[0])
+            group_name=x[1]
+            depth=100
+            file_name=file_dir+group_name + ".csv"
+            print(type,group_name,depth)
+            C = CollectPosts(ids=group_name, corpus_file=file_name, depth=depth)
+            C.login(email, password)
+            # user_id = C.create_original_link(url)
+            # g=C.scrape_genger(user_id)
+            # print("grnder:::",g)
+            C.collect("groups")
+            print("collection done from ", group_name)
+            object.append(C)
+            C.browser.close()
+            del C
+            # del C
         except Exception as e:
             print("type error: " + str(e))
             print(traceback.format_exc())
+    #
+    # while True:
+    #     try:
+    #         type = input("Enter 1 for scraping from Group or Enter 2 for scraping from Page: ")
+    #         if(type=="1"):
+    #             group_name=input("Enter Group Name:")
+    #             print("Depth:")
+    #             depth=int(input())
+    #             file_name=input("Enter the filename you want to save:")
+    #             file_name=file_dir+file_name + ".csv"
+    #             C = CollectPosts(ids=group_name,corpus_file=file_name ,depth=depth)
+    #             C.login(email, password)
+    #             # user_id = C.create_original_link(url)
+    #             # g=C.scrape_genger(user_id)
+    #             # print("grnder:::",g)
+    #             C.collect("groups")
+    #             # df=pd.read_csv(file_name)
+    #             # print(df.head())
+    #             # print(df['profileLink'])
+    #             # C.login(email, password)
+    #             # C.retrive_gender(df)
+    #             # C.browser.quit()
+    #             # for link in df['profileLink']:
+    #             #     user_id = C.create_original_link(str(link))
+    #             #     g=C.scrape_genger(user_id)
+    #             #     print("grnder:::",g)
+    #         if (type == "2"):
+    #             page_name = input("Enter Page Name:")
+    #             print("Depth:")
+    #             depth = int(input())
+    #             file_name = input("Enter the filename you want to save:")
+    #             file_name = file_dir+file_name + ".csv"
+    #             C = CollectPosts(ids=page_name,corpus_file=file_name ,depth=depth)
+    #             C.login(email, password)
+    #             C.collect("pages")
+    #     except Exception as e:
+    #         print("type error: " + str(e))
+    #         print(traceback.format_exc())
